7482cafc812fdee02123d6203f13f86b5b28bf91
Rushil Shakya <rushil.shakya@gmail.com>
2019-10-04 12:38:09 -0400

    new vote subscribe

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const Query = require('./resolvers/Query');
 const Mutation = require('./resolvers/Mutation');
 const User = require('./resolvers/User');
 const Link = require('./resolvers/Link');
+const Vote = require('./resolvers/Vote');
 const Subscription = require('./resolvers/Subscription');
 // let links = [{ id: 'link-0', url: 'www.google.com', description: 'google' }];
 // let idCount = links.length;
@@ -14,6 +15,7 @@ const resolvers = {
   Subscription,
   User,
   Link,
+  Vote,
 };
 
 // 3
